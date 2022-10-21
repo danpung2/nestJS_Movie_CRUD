0ea78c841bb2131473ca40a3f205d74412384966
danpung2 <danpung_2@naver.com>
2022-10-21 17:35:16 +0900

    unit test(jest): getAll, getOne

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
+import { NotFoundException } from "@nestjs/common";
 
 describe('MoviesService', () => {
   let service: MoviesService;
@@ -15,4 +16,33 @@ describe('MoviesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe("getAll", () => {
+    it("should return an array", () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    })
+  })
+
+  describe("getOne", () => {
+    it("should return a movie", () => {
+      service.create({
+        title: "Test Movie",
+        genres: ["test"],
+        year: 2022,
+      });
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie.title).toEqual("Test Movie");
+    })
+    it("should throw 404 error", () => {
+      try {
+        service.getOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual("Movie with id: 999 not found");
+      }
+    })
+  })
+
 });
