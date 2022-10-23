44babb459470c06b46c7bb3feb9b94ed8e0cd154
danpung2 <danpung_2@naver.com>
2022-10-23 17:20:01 +0900

    jest: delete, update

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -45,4 +45,60 @@ describe('MoviesService', () => {
     })
   })
 
+  describe("deleteOne", () => {
+    it("delete a movie", () => {
+      service.create({
+        title: "Test Movie",
+        genres: ["test"],
+        year: 2022,
+      });
+      const beforeDelete = service.getAll().length;
+      service.deleteOne(1);
+      const afterDelete = service.getAll().length;
+      expect(afterDelete).toBeLessThan(beforeDelete);
+    })
+    it("should return a 404", () => {
+      try{
+        service.deleteOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual("Movie with id: 999 not found");
+      }
+    });
+  });
+
+  describe("create", () => {
+    it("should create a movie", () => {
+      const beforeCreate = service.getAll().length;
+      service.create({
+        title: "Test Movie",
+        genres: ["test"],
+        year: 2022,
+      });
+      const afterCreate = service.getAll().length;
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
+    });
+  });
+
+  describe("update", () => {
+    it("should update a movie", () => {
+      service.create({
+        title: "Test Movie",
+        genres: ["test"],
+        year: 2022,
+      });
+      service.update(1, {title: "Updated Test"});
+      const movie = service.getOne(1);
+      expect(movie.title).toEqual("Updated Test");
+    });
+    it("should throw a NotFoundException", () => {
+      try{
+        service.update(999, {title: "Updated Test"});
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual("Movie with id: 999 not found");
+      }
+    });
+  });
+
 });
